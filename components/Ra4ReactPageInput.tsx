e1ee4891e968106d1aff6de7e29451170de6bcd8
Fauzan Lubis <fauzanfauh45@gmail.com>
2022-05-13 15:04:07 +0700

    first commit

---
+++ components/Ra4ReactPageInput.tsx
@@ -0,0 +1,43 @@
+import { Paper } from "@material-ui/core";
+import type { EditorProps } from "@react-page/editor";
+import Editor from "@react-page/editor";
+import React from "react";
+
+import { Labeled, useInput } from "react-admin";
+
+export type RaReactPageInputProps = {
+  label?: string;
+  source: string;
+  style?: React.CSSProperties;
+} & EditorProps;
+const RaReactPageInput: React.FC<RaReactPageInputProps> = ({
+  label = "Content",
+  source,
+  style,
+  ...editorProps
+}) => {
+  const {
+    field: { value, onChange },
+  } = useInput({ source });
+
+  return (
+    <Labeled label={label} source={source} fullWidth>
+      <>
+        <Paper
+          elevation={5}
+          style={{
+            overflow: "visible",
+            padding: 16,
+            marginRight: 64,
+
+            ...style,
+          }}
+        >
+          <Editor value={value} onChange={onChange} {...editorProps} />
+        </Paper>
+      </>
+    </Labeled>
+  );
+};
+
+export default RaReactPageInput;
---
+++ next.config.js
@@ -0,0 +1,31 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const path = require('path');
+module.exports = withBundleAnalyzer({
+  basePath: process.env.RELEASE_CHANNEL
+    ? !process.env.RELEASE_CHANNEL || process.env.RELEASE_CHANNEL === 'latest'
+      ? '/'
+      : '/' + process.env.RELEASE_CHANNEL
+    : undefined,
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/index.html',
+      },
+    ];
+  },
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react/jsx-runtime.js': 'react/jsx-runtime',
+      'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
+    };
+    return config;
+  },
+});
